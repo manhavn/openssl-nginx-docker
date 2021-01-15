8bd2250457f6398c805e1a5b0851666aeef57aa3
manhavn <manhavn@outlook.com>
2021-01-15 10:57:47 +0700

    mkdir lost folder

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+mkdir -p ssl/certs
+mkdir -p ssl/private
+mkdir -p nginx/parameters
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ssl/private/nginx-selfsigned.key -out ssl/certs/nginx-selfsigned.crt
 openssl dhparam -out nginx/parameters/dhparam.pem 4096
 docker-compose -f openssl_nginx.yml up -d
